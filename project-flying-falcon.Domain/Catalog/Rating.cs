d1968654730c97ba08dd9574f7dc2785a9703222
Kai Switzer <kaiswitzer@icloud.com>
2026-03-31 17:49:11 -0400

    complete catalog API endpoints

--- project-flying-falcon.Domain/Catalog/Rating.cs
+++ project-flying-falcon.Domain/Catalog/Rating.cs
@@ -1,14 +1,16 @@
 using System;
-
 namespace Flying.Falcon.Domain.Catalog
-//Changed from Jet.Piranha.Domain.Catalog to Flying.Falcon.Domain.Catalog
-
 {
     public class Rating
     {
+        public int Id { get; set; }
         public int Stars { get; set; }
         public string UserName { get; set; }
         public string Review { get; set; }
+
+        // Required by EF Core to load objects from the database
+        // Think of it like a no-arg constructor Java sometimes needs for frameworks
+        protected Rating() { }
 
         public Rating(int stars, string userName, string review)
         {
@@ -16,12 +18,10 @@ namespace Flying.Falcon.Domain.Catalog
             {
                 throw new ArgumentException("Star rating must be an integer of: 1, 2, 3, 4, 5.");
             }
-
             if (string.IsNullOrEmpty(userName))
             {
                 throw new ArgumentException("UserName cannot be null.");
             }
-
             this.Stars = stars;
             this.UserName = userName;
             this.Review = review;
