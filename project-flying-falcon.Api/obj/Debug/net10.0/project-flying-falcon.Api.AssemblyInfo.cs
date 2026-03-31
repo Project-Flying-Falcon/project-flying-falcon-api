d0b488ae7e86b4364cd70d9fea478fabd27fba18
quinnmoore54 <quinnmoore54@gmail.com>
2026-03-31 14:50:39 -0400

    Done with first 2 sections. On Page 11

--- project-flying-falcon.Api/obj/Debug/net10.0/project-flying-falcon.Api.AssemblyInfo.cs
+++ project-flying-falcon.Api/obj/Debug/net10.0/project-flying-falcon.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-flying-falcon.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba255a832522ef120905540d416a8717cdc1a074")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-flying-falcon.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-flying-falcon.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
