dae5f5063418dcdc71f1b4804851ae6fe0ffe2e7
Vikas Duvvuri <vikas.k.duvvuri@gmail.com>
2026-04-12 20:08:19 -0400

    Add unit tests for Rating and Item

---
+++ tests/project-flying-falcon.Domain.Tests/RatingTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Flying.Falcon.Domain.Catalog;
+
+namespace project_flying_falcon.Domain.Tests;
+
+[TestClass]
+public sealed class RatingTests
+{
+    [TestMethod]
+    public void Can_Create_New_Rating()
+    {
+        // Arrange
+        var rating = new Rating(1, "Mike", "Great fit!");
+
+        // Act (empty)
+
+        // Assert
+        Assert.AreEqual(1, rating.Stars);
+        Assert.AreEqual("Mike", rating.UserName);
+        Assert.AreEqual("Great fit!", rating.Review);
+    }
+
+    [TestMethod]
+    public void Cannot_Create_Rating_With_Invalid_Stars()
+    {
+        // Arrange & Assert
+        try
+        {
+            var rating = new Rating(0, "Mike", "Great fit!");
+            Assert.Fail("Expected ArgumentException was not thrown");
+        }
+        catch (ArgumentException)
+        {
+            // Test passes
+        }
+    }
+}
